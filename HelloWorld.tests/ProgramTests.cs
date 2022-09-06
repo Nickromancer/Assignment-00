f05e573eab79dc1f2f1723e02bdd4f12c49c10ec
OliFryser <94053214+OliFryser@users.noreply.github.com>
2022-09-06 13:15:54 +0200

    UI (#4)
    * Tested and implemented Is LeapYear
    
    * Console writes prompt tested
    
    * User interface created (currently untested)

--- HelloWorld.tests/ProgramTests.cs
+++ HelloWorld.tests/ProgramTests.cs
@@ -2,10 +2,13 @@ namespace HelloWorld.tests;
 
 public class ProgramTests
 {
+    /*
     [Fact]
     public void ConsolePrintsPrompt()
     {
         // Arrange
+        var reader = new StringReader("4");
+        Console.SetIn(reader);
         var writer = new StringWriter();
         Console.SetOut(writer);
 
@@ -18,4 +21,39 @@ public class ProgramTests
         // Assert
         Assert.Equal("Please Enter a Year:", output);
     }
+
+    /*
+    [Fact]
+    public void HasInput()
+    {
+        // Arrange
+        var reader = new StringReader("4");
+        Console.SetIn(reader);
+
+        // Act
+        var program = Assembly.LoadFrom("HelloWorld.dll");
+        program?.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        // Assert
+        Assert.Equal("4", input);
+    }
+
+    [Fact]
+    public void GivenInput4OutputsTrue() {
+         // Arrange
+        var reader = new StringReader("4");
+        Console.SetIn(reader);
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        var program = Assembly.LoadFrom("HelloWorld.dll");
+        program?.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        var output = writer.GetStringBuilder().ToString().Trim();
+        
+        // Assert
+        Assert.Equal("yay", output);
+    }
+    */
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Please Enter a Year:");
+﻿using HelloWorld;
+
+Console.WriteLine("Please Enter a Year:");
+var input = Console.ReadLine();
+var inputnumber = Int32.Parse(input);
+
+var calculator = new LeapYearCalculator();
+
+bool result = calculator.IsLeapYear(inputnumber);
+
+if(result) Console.WriteLine("yay"); 
+else Console.WriteLine("nay");
