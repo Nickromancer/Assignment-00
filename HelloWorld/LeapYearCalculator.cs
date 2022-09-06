cc9ec146f6450e3d60289603e968cb2f2939da27
oljh <oljh@itu.dk>
2022-09-06 13:41:22 +0200

    Error Handling implemented and partially tested

--- HelloWorld/LeapYearCalculator.cs
+++ HelloWorld/LeapYearCalculator.cs
@@ -4,6 +4,7 @@ public class LeapYearCalculator
 {
     public bool IsLeapYear(int year) 
     {
+        if(year < 1582) throw new ArgumentException("The year must be over 1581");
         if(year % 4 == 0) 
         {
             if(year % 100 == 0 && year % 400 != 0) return false;
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,11 +2,32 @@
 
 Console.WriteLine("Please Enter a Year:");
 var input = Console.ReadLine();
-var inputnumber = Int32.Parse(input);
+
+var inputnumber = 0;
+
+try 
+{
+    inputnumber = Int32.Parse(input);
+}
+catch (FormatException e) 
+{
+    Console.WriteLine(e.Message);
+    return;
+}
 
 var calculator = new LeapYearCalculator();
 
-bool result = calculator.IsLeapYear(inputnumber);
+bool result = false;
+
+try 
+{
+    result = calculator.IsLeapYear(inputnumber);
+}
+catch (ArgumentException e) {
+    Console.WriteLine(e.Message);
+    return;
+}
+
 
 if(result) Console.WriteLine("yay"); 
 else Console.WriteLine("nay");
